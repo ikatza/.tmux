70032f517a4fe78219db94eeb1c5ad20086493a6
Iker Loïc de Icaza Astiz <i.de-icaza-astiz@sussex.ac.uk>
2019-07-29 17:32:10 -0500

    Add setup.sh script to create link to home dir

---
+++ setup.sh
@@ -0,0 +1,19 @@
+ #!/bin/bash
+
+#this script will setup tmux specifics using the configs in this repo
+
+files="tmux.conf tmux.conf.local tmux"
+date=`date +"%Y%m%d"`
+
+for file in $files; do
+    if [ -h ~/.$file ]; then
+        unlink ~/.$file
+    else
+        if [ -f ~/.$file ]; then
+            echo "backing up ~/.$file to ~/.$file.bak$date"
+            mv ~/.$file ~/.$file.bak$date
+        fi
+    fi
+    echo "symlinking $PWD/$file to ~/.$file"
+    ln -s $PWD/$file ~/.$file
+done
